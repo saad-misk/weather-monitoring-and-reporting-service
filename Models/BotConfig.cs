5f82524139feea078b819cc54eb3890c0cc89663
Saad Misk <221191@ppu.edu.ps>
2024-09-30 16:45:06 +0300

    Add Model classes: WeatherState and BotConfig.

---
+++ Models/BotConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weatherMonitoringAndReportingService.Models
+{
+    public class BotConfig
+    {
+        public bool IsEnabled {  get; set; }
+        public double Threshold {  get; set; }
+        public string? Message { get; set; }
+
+    }
+}
---
+++ Models/WeatherState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weatherMonitoringAndReportingService.Models
+{
+    public class WeatherState
+    {
+        [Required]
+        public string? Location { get; set; }
+
+        [Required]
+        public double Temperature {  get; set; }
+
+        [Required]
+        public double Humidity {  get; set; }
+
+        public override string ToString()
+        {
+            return  $"Location: {Location}\n" +
+                    $"Temperature: {Temperature}\n" +
+                    $"Humidty: {Humidity}\n";
+        }
+
+    }
+}
