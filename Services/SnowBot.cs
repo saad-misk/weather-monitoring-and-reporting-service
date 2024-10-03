43d30a4c169dd5b0fb9b415ef8701602a8f12e1a
Saad Misk <221191@ppu.edu.ps>
2024-10-03 15:57:21 +0300

    Add Bots classes with the weatherBot interface

---
+++ Services/SnowBot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using weatherMonitoringAndReportingService.Models;
+
+namespace weatherMonitoringAndReportingService.Services
+{
+    public class SnowBot : IWeatherBot
+    {
+        public bool Enabled { get; set; }
+        public double TemperatureThreshold { get; set; }
+        public string? Message { get; set; }
+
+        public SnowBot(BotConfig botConfig)
+        {
+            Enabled = botConfig.IsEnabled;
+            TemperatureThreshold = botConfig.Threshold;
+            Message = botConfig.Message;
+        }
+        public void Update(WeatherState data)
+        {
+            if (Enabled && data.Temperature >= TemperatureThreshold)
+            {
+                ActivateBot();
+            }
+        }
+
+        private void ActivateBot()
+        {
+            Console.WriteLine("SnowBot activated!");
+            Console.WriteLine(Message);
+        }
+    }
+}
---
+++ Services/SunBot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using weatherMonitoringAndReportingService.Models;
+
+namespace weatherMonitoringAndReportingService.Services
+{
+    public class SunBot : IWeatherBot
+    {
+        public bool Enabled { get; set; }
+        public double TemperatureThreshold { get; set; }
+        public string? Message { get; set; }
+
+        public SunBot(BotConfig botConfig)
+        {
+            Enabled = botConfig.IsEnabled;
+            TemperatureThreshold = botConfig.Threshold;
+            Message = botConfig.Message;
+        }
+        public void Update(WeatherState data)
+        {
+            if (Enabled && data.Temperature > TemperatureThreshold)
+            {
+                ActivateBot();
+            }
+        }
+
+        private void ActivateBot()
+        {
+            Console.WriteLine("SunBot activated!");
+            Console.WriteLine(Message);
+        }
+    }
+}
