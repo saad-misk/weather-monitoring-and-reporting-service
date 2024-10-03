1530cc49d157879f80f987948ace864572719d9e
Saad Misk <221191@ppu.edu.ps>
2024-10-03 12:35:41 +0300

    Add BotConfigRepository, Add BotsConfig.json

---
+++ Repositories/BotConfigRepository.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace weatherMonitoringAndReportingService.Repositories
+{
+    public class BotConfigRepository
+    {
+        private static readonly BotConfigRepository _instance = new BotConfigRepository();
+
+        private const string _configFilePath = @"Config.json";
+
+        public static BotConfigRepository Instance { get { return _instance; } }
+
+        private BotConfigRepository() 
+        {
+            LoadConfigFromFile(_configFilePath);
+        }
+
+        private void LoadConfigFromFile(string configFilePath)
+        {
+            string configJson = File.ReadAllText(configFilePath);
+            _bots = JsonConvert.Dese
+        }
+    }
+}
